97e5d89172d4edbf41e89a4bf3d7b4884a188d62
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2024-01-10 19:16:22 -0500

    python-p3-sequences/
    python-p3-sequences/

--- sequences.py
+++ sequences.py
@@ -1,24 +1,20 @@
 #!/usr/bin/env python3
+
 def print_fibonacci(length):
     if length <= 0:
-        return
+        print([])
     elif length == 1:
         print([0])
-        return
     elif length == 2:
         print([0, 1])
-        return
+    else:
+        fib_sequence = [0, 1]
+        num1 = 0
+        num2 = 1
 
-    fib_sequence = [0, 1]
-    num1 = 0
-    num2 = 1
+        for _ in range(2, length):
+            next_num = num1 + num2
+            fib_sequence.append(next_num)
+            num1, num2 = num2, next_num
 
-    for _ in range(2, length):
-        next_num = num1 + num2
-        fib_sequence.append(next_num)
-        num1, num2 = num2, next_num
-
-    print(fib_sequence)
-
-
-print_fibonacci(9)
+        print(fib_sequence)
